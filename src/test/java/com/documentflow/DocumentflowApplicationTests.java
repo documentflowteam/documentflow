5b8152c3d55a69578c6403e2d58ff0a59d800990
zefirka723 <manglee@mail.ru>
2020-02-02 02:32:44 +0300

    initial mvn project commit

---
+++ src/test/java/com/documentflow/DocumentflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.documentflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
