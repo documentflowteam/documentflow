c28aa43d0529f4bf4d52dac596dce730822a2a96
zefirka723 <45363989+zefirka723@users.noreply.github.com>
2020-02-04 00:08:28 +0300

    del state-repo
    чужой модуль

--- src/main/java/com/documentflow/repositories/StateRepository.java
+++
@@ -1,9 +0,0 @@
-package com.documentflow.repositories;
-
-import com.documentflow.entities.DocIn;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StateRepository extends JpaRepository<DocIn, Long> {
-}
