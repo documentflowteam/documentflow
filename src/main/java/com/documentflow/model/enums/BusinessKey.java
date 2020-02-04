a8fe699001e4ae7099aae38bafb54381aeb97f80
aleksey mishanin <mishanin_a_a@mail.ru>
2020-02-04 23:13:31 +0300

    added enum for the business logic

---
+++ src/main/java/com/documentflow/model/enums/BusinessKey.java
@@ -0,0 +1,19 @@
+package com.documentflow.model.enums;
+
+/**
+ * Statuses of business processes.
+ */
+public enum BusinessKey {
+
+    REGISTRATED,
+    EXECUTION,
+    EXECUTED,
+    DELETED,
+    RECALLED,
+    PROJECT,
+    APPROVING,
+    APPROVED,
+    SENT,
+    REWORK,
+    CHECKING
+}
