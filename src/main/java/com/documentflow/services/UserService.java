d786ef8b0dad575891c882bdf0d1eec280b84220
drsdgdby <nolifeirl@2ch.hk>
2020-02-08 16:15:06 +0300

    user-repository and user-service added

--- src/main/java/com/documentflow/services/UserService.java
+++ src/main/java/com/documentflow/services/UserService.java
@@ -2,11 +2,19 @@ package com.documentflow.services;
 
 import com.documentflow.entities.Department;
 import com.documentflow.entities.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
 
 public interface UserService {
-    User getUserById(int userId);
+    User getCurrentUser(int userId);
 
-    User getBossByUserId(int userId);
+    User getBoss(int userId);
 
     Department getDepartmentByUserId(int userId);
+
+    List<User> getAllUsers();
+
+    Page<User> getPageOfUsersBySpecification(Pageable pageable);
 }
--- src/main/java/com/documentflow/services/UserServiceImpl.java
+++ src/main/java/com/documentflow/services/UserServiceImpl.java
@@ -5,7 +5,11 @@ import com.documentflow.entities.User;
 import com.documentflow.repositories.UserRepository;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -13,17 +17,27 @@ public class UserServiceImpl implements UserService {
     private UserRepository userRepository;
 
     @Override
-    public User getUserById(int userId) {
+    public User getCurrentUser(int userId) {
         return userRepository.findOneById(userId);
     }
 
     @Override
-    public User getBossByUserId(int userId) {
+    public User getBoss(int userId) {
         return userRepository.findOneById(userId).getBoss();
     }
 
     @Override
     public Department getDepartmentByUserId(int userId) {
         return userRepository.findOneById(userId).getDepartment();
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public Page<User> getPageOfUsersBySpecification(Pageable pageable) {
+        return userRepository.findAll(pageable);
     }
 }
