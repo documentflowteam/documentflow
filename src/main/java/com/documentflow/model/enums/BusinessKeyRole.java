aa8378d6a3302476b64008dd313d9168f8560022
drsdgdby <nolifeirl@2ch.hk>
2020-02-13 16:53:24 +0300

    authorization added.

--- src/main/java/com/documentflow/model/enums/BusinessKeyRole.java
+++ src/main/java/com/documentflow/model/enums/BusinessKeyRole.java
@@ -1,15 +1,21 @@
 package com.documentflow.model.enums;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public enum BusinessKeyRole {
-    DOC_IN_REGISTRATION,
-    DOC_IN_TASK_CREATION,
-    DOC_OUT_CREATION,
-    DOC_IN_DELETING,
-    DOC_OUT_DELETING,
-    DOC_IN_READ,
-    DOC_OUT_READ,
-    TASKS_READ,
-    DOC_OUT_SENDING,
-    SYS_READ,
-    SYS_EDIT
+
+    DOC_IN_REGISTRATION("DOC_IN_REGISTRATION"),
+    DOC_IN_TASK_CREATION("DOC_IN_TASK_CREATION"),
+    DOC_OUT_CREATION("DOC_OUT_CREATION"),
+    DOC_IN_DELETING("DOC_IN_DELETING"),
+    DOC_OUT_DELETING("DOC_OUT_DELETING"),
+    DOC_IN_READ("DOC_IN_READ"),
+    DOC_OUT_READ("DOC_OUT_READ"),
+    TASKS_READ("TASKS_READ"),
+    DOC_OUT_SENDING("DOC_OUT_SENDING"),
+    SYS_READ("SYS_READ"),
+    SYS_EDIT("SYS_EDIT");
+
+    private final String name;
 }
