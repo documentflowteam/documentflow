aef1146cb382329911398f031694addce520f2dc
drsdgdby <nolifeirl@2ch.hk>
2020-02-14 21:37:35 +0300

    logout work

--- src/main/java/com/documentflow/security/SecurityConfig.java
+++ src/main/java/com/documentflow/security/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -46,13 +47,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter implements WebM
                 .loginProcessingUrl("/perform_login").permitAll()
                 .and()
                 .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .invalidateHttpSession(true)
                 .deleteCookies("JSESSIONID")
-                .logoutUrl("/perform_logout")
                 .logoutSuccessUrl("/login")
                 .permitAll()
                 .and()
                 .exceptionHandling()
-                .accessDeniedPage("/403.html");
+                .accessDeniedPage("/403");
     }
 
     @Bean
