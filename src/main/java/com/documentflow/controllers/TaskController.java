d6332c04a636d32581475e159725ff3302b8f241
Usman Umarov <usman@umarov.ru>
2020-02-21 00:18:53 +0300

    add Task registry template, form template and basic functionality

--- src/main/java/com/documentflow/controllers/TaskController.java
+++ src/main/java/com/documentflow/controllers/TaskController.java
@@ -1,12 +1,8 @@
 package com.documentflow.controllers;
 
-import com.documentflow.entities.DocIn;
-import com.documentflow.entities.DocOut;
-import com.documentflow.entities.Task;
-import com.documentflow.services.DocInService;
-import com.documentflow.services.DocOutService;
-import com.documentflow.services.TaskService;
-import com.documentflow.services.TaskTypeService;
+import com.documentflow.entities.*;
+import com.documentflow.model.enums.BusinessKeyTask;
+import com.documentflow.services.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
@@ -14,12 +10,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.List;
 
 @Controller
 @RequestMapping("/tasks")
 public class TaskController {
+    private UserServiceImpl userService;
     private TaskService taskService;
+    private TaskHistoryService taskHistoryService;
     private TaskTypeService taskTypeService;
     private DocInService docInService;
     private DocOutService docOutService;
@@ -30,8 +29,28 @@ public class TaskController {
     }
 
     @Autowired
+    public void setTaskHistoryService(TaskHistoryService taskHistoryService) {
+        this.taskHistoryService = taskHistoryService;
+    }
+
+    @Autowired
     public void setTaskTypeService(TaskTypeService taskTypeService) {
         this.taskTypeService = taskTypeService;
+    }
+
+    @Autowired
+    public void setUserService(UserServiceImpl userService) {
+        this.userService = userService;
+    }
+
+    @Autowired
+    public void setDocOutService(DocOutService docOutService) {
+        this.docOutService = docOutService;
+    }
+
+    @Autowired
+    public void setDocInService(DocInService docInService) {
+        this.docInService = docInService;
     }
 
     @GetMapping("")
@@ -40,14 +59,20 @@ public class TaskController {
     }
 
     @GetMapping("/registry/{direction}")
-    public String showTasks(Model model, @PathVariable String direction) {
+    public String showTasks(Principal principal, Model model, @PathVariable String direction) {
+        User user = null;
+        if (principal != null) {
+            user = userService.getUserByUsername(principal.getName());
+        }
         if (direction.equals("in")) {
-            List<Task> tasks = taskService.findAll(PageRequest.of(0, 10, Sort.Direction.ASC, "id")).getContent();
+            //List<Task> tasks = taskService.findAll(PageRequest.of(0, 10, Sort.Direction.ASC, "id")).getContent();
+            List<Task> tasks = taskService.findAllByExecutor(user);
             model.addAttribute("direction", direction);
             model.addAttribute("tasks", tasks);
             return "tasks_registry";
         } else if (direction.equals("out")) {
-            List<Task> tasks = taskService.findAll(PageRequest.of(0, 10, Sort.Direction.ASC, "id")).getContent();
+            //List<Task> tasks = taskService.findAll(PageRequest.of(0, 10, Sort.Direction.ASC, "id")).getContent();
+            List<Task> tasks = taskService.findAllByAuthor(user);
             model.addAttribute("direction", direction);
             model.addAttribute("tasks", tasks);
             return "tasks_registry";
@@ -67,17 +92,31 @@ public class TaskController {
     }
 
     @GetMapping("/card/{id}")
-    public String showTask(Model model, @PathVariable Long id) {
+    public String showTask(Principal principal, Model model, @PathVariable Long id) {
+        User user = null;
+        if (principal != null) {
+            user = userService.getUserByUsername(principal.getName());
+        }
+
         Task task = taskService.findOneById(id);
+        TaskHistory newTaskHistory = new TaskHistory();
+        List<TaskHistory> taskHistory = taskHistoryService.findAllByTask(task);
+
         String taskType = task.getTaskType().getBusinessKey();
-//        if (taskType.equals("EXECUTION")) {
-//            DocIn docIn = docInService.findByTask(task);
-//            model.addAttribute("docIn", docIn);
-//        } else if (taskType.equals("APPROVING")) {
-//            DocOut docOut = docOutService.findByTask(task);
-//            model.addAttribute("docOut", docOut);
-//        }
+        if (taskType.equals(BusinessKeyTask.EXECUTION.name())) {
+            DocIn docIn = docInService.findById(1L);
+            model.addAttribute("docIn", docIn);
+        } else if (taskType.equals(BusinessKeyTask.APPROVING.name())) {
+            DocOut docOut = docOutService.findOneById(1L);
+            model.addAttribute("docOut", docOut);
+        }
+
+        newTaskHistory.setUser(user);
+        newTaskHistory.setTask(task);
+
         model.addAttribute("task", task);
+        model.addAttribute("taskHistory", taskHistory);
+        model.addAttribute("newTaskHistory", newTaskHistory);
         return "task";
     }
 
@@ -85,6 +124,15 @@ public class TaskController {
     public String saveTask(@ModelAttribute(name = "task") Task task) {
         taskService.save(task);
         return "redirect:/docs/in";
+    }
+
+    @PostMapping("/save/history")
+    public String saveTaskHistory(@ModelAttribute(name = "newTaskHistory") TaskHistory taskHistory) {
+        Task task = taskHistory.getTask();
+        task.setState(taskHistory.getState());
+        taskService.save(task);
+        taskHistoryService.save(taskHistory);
+        return "redirect:/tasks/";
     }
 
 
