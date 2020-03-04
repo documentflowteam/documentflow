d148a5773ff2597dde17802aad299675b52f240c
aleksey mishanin <mishanin_a_a@mail.ru>
2020-03-05 00:14:32 +0300

    fix

--- src/test/java/com/documentflow/controllers/ContragentControllerTest.java
+++ src/test/java/com/documentflow/controllers/ContragentControllerTest.java
@@ -1,6 +1,12 @@
 package com.documentflow.controllers;
 
 import com.documentflow.AbstractDocumentFlowTest;
+import org.junit.Test;
 
 public class ContragentControllerTest extends AbstractDocumentFlowTest {
+
+    @Test
+    public void test(){
+        //TODO create test for controller
+    }
 }
